4f4ddc1a582a2b9b1d4498b182c0f11740b433ef
Shyunju <hyunju9908@naver.com>
2025-05-20 15:56:11 +0900

    also player2

--- Assets/Scripts/Player/Player2InputController.cs
+++ Assets/Scripts/Player/Player2InputController.cs
@@ -8,19 +8,23 @@ namespace CookCo_opGame
         [SerializeField] InputActionAsset _playerInputActions;
         InputActionMap _player2Map;
         InputAction _player2MoveAction;
+        InputAction _player2PickAction;
+        InputAction _player2DashAction;
+        InputAction _player2ThrowAction;
         public override void OnEnable()
         {
             _player2Map = _playerInputActions.FindActionMap("Player2Actions");
             _player2Map.Enable();
 
             _player2MoveAction = _player2Map.FindAction("Move");
-            _player2MoveAction = _player2Map.FindAction("Pick");
-            _player2MoveAction = _player2Map.FindAction("Dash");
-            _player2MoveAction = _player2Map.FindAction("Throw");
+            _player2PickAction = _player2Map.FindAction("Pick");
+            _player2DashAction = _player2Map.FindAction("Dash");
+            _player2ThrowAction = _player2Map.FindAction("Throw");
 
 
             _player2MoveAction.performed += OnPlayerMove;
             _player2MoveAction.canceled += OnPlayerMove;
+            _player2PickAction.performed += OnPlayerPick;
 
             _player2MoveAction.Enable();
         }
