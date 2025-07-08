95a28ebd48781fefd23fce79f1b9abe1a29ecba9
Shyunju <hyunju9908@naver.com>
2025-07-08 18:49:36 +0900

    make recipe json

---
+++ Assets/Scripts/Item/RecipeData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+namespace CookCo_opGame
+{
+    public class RecipeData
+    {
+        public int RecipeID;
+        public string RecipeName;
+        public string UIPath;
+        [System.NonSerialized]
+        public Sprite UISprite;
+
+        [System.Serializable]
+        public class RecipeDataList
+        {
+            public List<RecipeData> Recipes = new List<RecipeData>();
+        }
+    }
+}
