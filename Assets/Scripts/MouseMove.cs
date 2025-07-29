8a2285dc1368d0665820cfb1fc3a8c54b2e81660
Shyunju <hyunju9908@naver.com>
2025-07-29 18:53:18 +0900

    mouse nav test

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace CookCo_opGame
 {
     public class MouseMove : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public Transform target;  // 이동할 목표 위치(음식 위치 등)
+
+        private NavMeshAgent agent;
+
         void Start()
         {
-        
+            agent = GetComponent<NavMeshAgent>(); // NavMeshAgent 컴포넌트 가져오기
+            if (target != null)
+            {
+                agent.SetDestination(target.position); // 목적지 설정
+            }
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            if (target != null && agent.destination != target.position)
+            {
+                agent.SetDestination(target.position); // 타겟 위치가 변하면 계속 업데이트 가능
+            }
         }
     }
 }
