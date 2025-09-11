0b4924c37e0576dd891311438c6ea328180e5f8d
Shyunju <hyunju9908@naver.com>
2025-09-11 18:41:42 +0900

    20250911 마무리

--- Assets/Scripts/SaveAndLoad/DataManager.cs
+++ Assets/Scripts/SaveAndLoad/DataManager.cs
@@ -4,6 +4,10 @@ namespace CookCo_opGame
 {
     public class DataManager : Singleton<DataManager>
     {
-
+        PlayerData _playerData = new PlayerData();
+        void Start()
+        {
+            _playerData.Name = "testName";
+        }
     }
 }
--- Assets/Scripts/SaveAndLoad/PlayerData.cs
+++ Assets/Scripts/SaveAndLoad/PlayerData.cs
@@ -1,13 +1,19 @@
-using UnityEngine;
-
+using System.Collections.Generic;
 namespace CookCo_opGame
 {
-    public class PlayerData : MonoBehaviour
+    [System.Serializable]
+    public class PlayerData
     {
         //이름, 지갑, 누적금, 테이블 구매 현황
         public string Name;
         public int Wallet;
         public int Aggregate;
         public SellingTable[] SellingTable;
+
+    }
+    [System.Serializable]
+    public class PlayerDataList
+    {
+        public List<PlayerData> Items = new List<PlayerData>();
     }
 }
