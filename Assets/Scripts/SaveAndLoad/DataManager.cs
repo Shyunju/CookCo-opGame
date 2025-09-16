98bc28cc3d546ee3bddbe3081940f844d60f3dc1
Shyunju <hyunju9908@naver.com>
2025-09-16 14:52:46 +0900

    make select file UI

--- Assets/Scripts/SaveAndLoad/DataManager.cs
+++ Assets/Scripts/SaveAndLoad/DataManager.cs
@@ -1,44 +1,39 @@
 using UnityEngine;
 using System.IO;
-using System;
-using Unity.VisualScripting;
 namespace CookCo_opGame
 {
     public class DataManager : Singleton<DataManager>
     {
-        PlayerData _nowPlayer = new PlayerData();
-        string _path;
-        string _fileName = "save";
+        PlayerData _nowPlayer = new PlayerData(); // 플레이어 데이터 생성
 
+        public string Path { get; set; } // 경로
+        public int NowSlot { get; set; } // 현재 슬롯번호
 
-        void Awake()
+        public PlayerData NowPlayer {get{return _nowPlayer;} set { _nowPlayer = value; }}
+
+        private void Awake()
         {
             base.Awake();
-            _path = Application.persistentDataPath + "/";
+            Path = Application.persistentDataPath + "/save";	// 경로 지정
+            print(Path);
         }
 
-        public void SaveData() // 새로운 파일 생성과 기존 파일 덮어쓰기 구분하기
+        public void SaveData()
         {
-            if (!File.Exists(_path + _fileName)) //같은 이름의 파일이 없음 = 생성 가능
-            {
-                string data = JsonUtility.ToJson(_nowPlayer);
-                File.WriteAllText(_path + _fileName, data);   //어디에 무엇을                
-            }
-            else  //있다면 생성 불가 혹은 덮어쓰기
-            {
-
-            }
-
+            string data = JsonUtility.ToJson(_nowPlayer);
+            File.WriteAllText(Path+ NowSlot.ToString(), data);
         }
+
         public void LoadData()
         {
-            string data = File.ReadAllText(_path + _fileName);
+            string data = File.ReadAllText(Path + NowSlot.ToString());
             _nowPlayer = JsonUtility.FromJson<PlayerData>(data);
         }
-        public void InputFileName(string fileName)
+
+        public void DataClear()
         {
-            _fileName = fileName;
-            SaveData();
+            NowSlot = -1;
+            _nowPlayer = new PlayerData();
         }
     }
 }
--- Assets/Scripts/SaveAndLoad/SelectFile.cs
+++ Assets/Scripts/SaveAndLoad/SelectFile.cs
@@ -1,19 +1,67 @@
+using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace CookCo_opGame
 {
     public class SelectFile : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        [SerializeField] GameObject _creat;	// 플레이어 닉네임 입력UI
+        [SerializeField] TMP_Text[] _slotText;		// 슬롯버튼 아래에 존재하는 Text들
+        [SerializeField] TMP_Text _newPlayerName;	// 새로 입력된 플레이어의 닉네임
+
+        bool[] _savefile = new bool[3];	// 세이브파일 존재유무 저장
+
         void Start()
         {
-        
+            // 슬롯별로 저장된 데이터가 존재하는지 판단.
+            for (int i = 0; i < 3; i++)
+            {
+                if (File.Exists(DataManager.Instance.Path + $"{i}"))	// 데이터가 있는 경우
+                {
+                    _savefile[i] = true;			// 해당 슬롯 번호의 bool배열 true로 변환
+                    DataManager.Instance.NowSlot = i;	// 선택한 슬롯 번호 저장
+                    DataManager.Instance.LoadData();	// 해당 슬롯 데이터 불러옴
+                    _slotText[i].text = DataManager.Instance.NowPlayer.Name;	// 버튼에 닉네임 표시
+                }
+                else	// 데이터가 없는 경우
+                {
+                    _slotText[i].text = "비어있음";
+                }
+            }
+            // 불러온 데이터를 초기화시킴.(버튼에 닉네임을 표현하기위함이었기 때문)
+            DataManager.Instance.DataClear();	
         }
 
-        // Update is called once per frame
-        void Update()
+        public void Slot(int number)	// 슬롯의 기능 구현
         {
-        
+            DataManager.Instance.NowSlot = number;	// 슬롯의 번호를 슬롯번호로 입력함.
+
+            if (_savefile[number])	// bool 배열에서 현재 슬롯번호가 true라면 = 데이터 존재한다는 뜻
+            {
+                DataManager.Instance.LoadData();	// 데이터를 로드하고
+                GoGame();	// 게임씬으로 이동
+            }
+            else	// bool 배열에서 현재 슬롯번호가 false라면 데이터가 없다는 뜻
+            {
+                Creat();	// 플레이어 닉네임 입력 UI 활성화
+            }
+        }
+
+        public void Creat()	// 플레이어 닉네임 입력 UI를 활성화하는 메소드
+        {
+            _creat.gameObject.SetActive(true);
+        }
+
+        public void GoGame()	// 게임씬으로 이동
+        {
+            if (!_savefile[DataManager.Instance.NowSlot])	// 현재 슬롯번호의 데이터가 없다면
+            {
+                DataManager.Instance.NowPlayer.Name = _newPlayerName.text; // 입력한 이름을 복사해옴
+                DataManager.Instance.SaveData(); // 현재 정보를 저장함.
+            }
+            SceneManager.LoadScene("LobbyScene"); // 게임씬으로 이동
         }
     }
 }
